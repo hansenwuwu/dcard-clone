da7f2b2a767c3ab640e0e4aacf0749ac0efe1f04
Hs <eric248655665@gmail.com>
2020-09-02 09:22:53 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dcard clone</h1>
+      {/* flex box column around */}
+      {/* Header */}
+      {/* Body */}
+    </div>
+  );
+}
+
+export default App;
