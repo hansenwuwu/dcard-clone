f51d5c2af138b5b3ebd0a47fb372e23220d5f0f0
Hs <hansenwu@micb2b.com>
2021-01-05 12:32:30 +0800

    ➕ Add modal

--- src/component/Body/Forum/Forum.js
+++ src/component/Body/Forum/Forum.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './Forum.css';
 
@@ -6,24 +6,64 @@ import ForumBody from './ForumBody';
 import ForumNavigator from './ForumNavigator';
 import Commercial from './Commercial';
 
+import Modal from '@material-ui/core/Modal';
 import {
     useLocation,
+    useHistory,
 } from "react-router-dom";
 
 function Forum(props) {
 
+    let history = useHistory();
     let location = useLocation();
 
     const { state = {} } = location;
     const { modal } = state;
 
+    const [open, setOpen] = React.useState(false);
+
+    useEffect(() => {
+        if (modal) {
+            handleOpen();
+            console.log("open");
+        }
+    }, [modal])
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        history.goBack();
+        console.log(location.pathname);
+    };
+
+    const body = (
+        <div >
+            <h2 id="simple-modal-title">Text in a modal</h2>
+            <p id="simple-modal-description">
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+            {/* <Forum /> */}
+        </div>
+    );
+
     return (
         <div className="forum__container">
-            {/* { modal && <h1>this is modal</h1>} */}
 
             <ForumNavigator />
             <ForumBody />
             <Commercial />
+
+            { modal && <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {body}
+            </Modal>}
 
         </div>
     );
